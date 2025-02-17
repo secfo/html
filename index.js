ce4e7c89bb8da545649cf6678eaff1a3b024de08
secfo <85417939+secfo@users.noreply.github.com>
2025-02-17 21:00:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,44 @@
-var Typer = {
-  text: '',
-  accessCountimer: null,
-  index: 0,
-  speed: 2,
-  file: '',
-  accessCount: 0,
-  deniedCount: 0,
-  init: function () {
-    accessCountimer = setInterval(function () {
-      Typer.updLstChr();
-    }, 500);
-    $.get(Typer.file, function (data) {
-      Typer.text = data;
-      Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-    });
-  },
+document.addEventListener("DOMContentLoaded", function () {
+    var Typer = {
+        text: '',
+        index: 0,
+        speed: 50,
+        file: 'index.txt',
 
-  content: function () {
-    return $('#console').html();
-  },
+        init: function () {
+            fetch(Typer.file)
+                .then(response => response.text())
+                .then(data => {
+                    Typer.text = data.trim();
+                    Typer.startTyping();
+                })
+                .catch(error => console.error("Error loading file:", error));
+        },
 
-  write: function (str) {
-    $('#console').append(str);
-    return false;
-  },
+        content: function () {
+            return document.getElementById("console").innerHTML;
+        },
 
-  addText: function (key) {
-    if (key.keyCode == 18) {
-      Typer.accessCount++;
+        write: function (str) {
+            document.getElementById("console").innerHTML += str;
+        },
 
-      if (Typer.accessCount >= 3) {
-        Typer.makeAccess();
-      }
-    } else if (key.keyCode == 20) {
-      Typer.deniedCount++;
+        addText: function () {
+            if (Typer.index < Typer.text.length) {
+                let char = Typer.text.charAt(Typer.index);
+                Typer.index++;
+                if (char === "\n") {
+                    char = "<br/>";
+                }
+                Typer.write(char);
+                setTimeout(Typer.addText, Typer.speed);
+            }
+        },
 
-      if (Typer.deniedCount >= 3) {
-        Typer.makeDenied();
-      }
-    } else if (key.keyCode == 27) {
-      Typer.hidepop();
-    } else if (Typer.text) {
-      var cont = Typer.content();
-      if (cont.substring(cont.length - 1, cont.length) == '|')
-        $('#console').html(
-          $('#console')
-            .html()
-            .substring(0, cont.length - 1),
-        );
-      if (key.keyCode != 8) {
-        Typer.index += Typer.speed;
-      } else {
-        if (Typer.index > 0) Typer.index -= Typer.speed;
-      }
-      var text = Typer.text.substring(0, Typer.index);
-      var rtn = new RegExp('\n', 'g');
+        startTyping: function () {
+            setTimeout(Typer.addText, Typer.speed);
+        }
+    };
 
-      $('#console').html(text.replace(rtn, '<br/>'));
-      window.scrollBy(0, 50);
-    }
-
-    if (key.preventDefault && key.keyCode != 122) {
-      key.preventDefault();
-    }
-
-    if (key.keyCode != 122) {
-      // other way prevent keys default behavior
-      key.returnValue = false;
-    }
-  },
-
-  updLstChr: function () {
-    var cont = this.content();
-
-    if (cont.substring(cont.length - 1, cont.length) == '|')
-      $('#console').html(
-        $('#console')
-          .html()
-          .substring(0, cont.length - 1),
-      );
-    else this.write('|'); // else write it
-  },
-};
-
-function replaceUrls(text) {
-  return text; // Simply return the text without modifying URLs
-}
-
-Typer.speed = 3;
-Typer.file = 'index.txt';
-Typer.init();
-
-var timer = setInterval('t();', 30);
-function t() {
-  Typer.addText({ keyCode: 123748 });
-
-  if (Typer.index > Typer.text.length) {
-    clearInterval(timer);
-  }
-}
+    Typer.init();
+});
